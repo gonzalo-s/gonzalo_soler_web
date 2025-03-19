18410e0a5314e92edf89aa6b1b5a5bd74d1b776b
gonzalo <gonzalosoler@gmail.com>
2025-03-19 17:55:34 -0300

    feat(components): add mobile and desktop navigation and styling

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,6 +1,11 @@
+'use client';
+
 import { Sections } from '@/constants/sections';
 import Button, { ButtonProps } from '../Button/Button';
 import styles from './navigation.module.scss';
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import { ICONS } from '@/constants/icons';
 
 type NavigationProps = {
   logo: ButtonProps;
@@ -11,7 +16,47 @@ function Navigation(props: NavigationProps) {
   return (
     <nav className={styles.navigation}>
       <Button {...props.logo} />
-      <div className={styles.links}>
+      <DesktopNavigation {...props} />
+      <MobileNavigation {...props} />
+    </nav>
+  );
+}
+
+export default Navigation;
+
+function DesktopNavigation(props: NavigationProps) {
+  return (
+    <div className={styles.desktop}>
+      {props.linkList.map((listLink) => {
+        return (
+          <Button
+            key={listLink.title}
+            text={listLink.title}
+            variant={listLink.buttonVariant}
+            icon={listLink.icon}
+            href={listLink.href}
+          />
+        );
+      })}
+    </div>
+  );
+}
+function MobileNavigation(props: NavigationProps) {
+  // TODO: handle outside click, handle scroll => close menu, etc
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  function toggleMenu() {
+    setShowMenu(!showMenu);
+  }
+
+  return (
+    <div className={styles.mobile}>
+      <button className={styles.mobile__button} onClick={toggleMenu}>
+        {showMenu ? ICONS.menuClose : ICONS.menuOpen}
+      </button>
+
+      <div className={clsx(styles.mobile__menu, showMenu ? styles.mobile__menu__show : styles.mobile__menu__hide)}>
         {props.linkList.map((listLink) => {
           return (
             <Button
@@ -24,8 +69,6 @@ function Navigation(props: NavigationProps) {
           );
         })}
       </div>
-    </nav>
+    </div>
   );
 }
-
-export default Navigation;
--- src/constants/icons.tsx
+++ src/constants/icons.tsx
@@ -1,8 +1,10 @@
-import { FaLinkedin, FaGithub, FaLongArrowAltRight, FaCode } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaLongArrowAltRight, FaCode, FaBars, FaSlash } from 'react-icons/fa';
 
 export const ICONS = {
   linkedin: <FaLinkedin />,
   github: <FaGithub />,
   arrowAltRight: <FaLongArrowAltRight style={{ transform: 'rotate(-45deg)' }} />,
   logo: <FaCode />,
+  menuOpen: <FaBars />,
+  menuClose: <FaSlash />,
 };
