dc6bea092da9b455d23f1667d2831e2d646e6bfb
gonzalo <gonzalosoler@gmail.com>
2025-04-20 10:02:29 -0300

    fix(Loading): add loading prop to PacmanLoader for proper loading state indication

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,11 @@
+'use client';
 import { PacmanLoader } from 'react-spinners';
 import styles from './layout.module.scss';
 
 export default function Loading() {
   return (
     <div className={styles.loading}>
-      <PacmanLoader color={'#757575'} />
+      <PacmanLoader color={'#757575'} loading />
     </div>
   );
 }
